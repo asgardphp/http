5cc0ca80152986e146474bac2cc4e89db29a64f9
Michel Hognerud <leyou.m@gmail.com>
2014-06-28 21:55:30 +0800

    fixes

--- tests/Fixtures/HomeController.php
+++ tests/Fixtures/HomeController.php
@@ -8,4 +8,12 @@ class HomeController extends \Asgard\Http\Controller {
 	public function homeAction($request) {
 		return '<h1>Asgard</h1><p>Hello!</p>';
 	}
+
+	public function errorAction() {
+		echo $a;
+	}
+
+	public function exceptionAction() {
+		throw new \Asgard\Http\Tests\NotFoundException;
+	}
 }
